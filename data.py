f322590a95b5c165448ef9529b44ab56f91fb79f
JooElkh <157903989+JooElkh@users.noreply.github.com>
2024-01-28 16:16:51 +0300

    Update data.py

--- data.py
+++ data.py
@@ -1,13 +1,13 @@
 # Елховский Иосиф, 12-я когорта — Финальный проект. Инженер по тестированию плюс
 
 order_body = {
-        "firstName": "Миша",
-        "lastName": "Всехуйня",
-        "address": "Konoha, 142 apt.",
-        "metroStation": 4,
-        "phone": "+7 800 355 35 35",
+        "firstName": "Иосиф",
+        "lastName": "Елховский",
+        "address": "Лечебный переулок, 2а",
+        "metroStation": 44,
+        "phone": "+7 962 881 28 32",
         "rentTime": 5,
-        "deliveryDate": "2020-06-06",
-        "comment": "Saske, come back to Konoha",
+        "deliveryDate": "2024-01-28",
+        "comment": "Не звонить",
         "color": ["BLACK"]
     }
